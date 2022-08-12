56a13ca0f6eb64157ff9a3ad96fd5d262bbda774
Dislate <ice_balyk@mail.ru>
2022-08-12 17:38:40 +0300

    docker psql + models sqlAlchemy + .env_dev

--- core/config.py
+++ core/config.py
@@ -1,6 +1,6 @@
 from starlette.config import Config
 
 
-config = Config(".env")
+config = Config(".env_dev")
 
 DATABASE_URL = config("EE_DATABASE_URL", cast=str, default="")
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from db.base import db
 import uvicorn
 
 
@@ -8,6 +9,16 @@ app = FastAPI()
 @app.get('/')
 async def root():
     return {"message": "Hello World"}
+
+
+@app.on_event("startup")
+async def startup():
+    await db.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await db.disconnect()
 
 
 if __name__ == "__main__":
